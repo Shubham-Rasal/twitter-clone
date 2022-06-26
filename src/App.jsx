9c560031d1fe4fefb533b7e29b5160a2b250c61e
Shubham-Rasal <bluequbits@gmail.com>
2022-06-26 22:37:26 +0530

    Adding auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,50 @@ import Tweet from './components/Tweet'
 import Feed from './components/Feed'
 import { ContextProvider } from './components/context'
 import './app.css'
+
+
+import { ContextHolder } from '@frontegg/rest-api';
+import { useAuth, useLoginWithRedirect } from "@frontegg/react";
+import { AdminPortal } from '@frontegg/react'
+
 function App() {
+
+  const { user, isAuthenticated } = useAuth();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
+  const handleClick = () => {
+    AdminPortal.show();
+  };
+  
 
 
   return (
     <div>
       <ContextProvider>
+      { isAuthenticated ? (
+        <div>
+          <div>
+            <img src={user?.profilePictureUrl} alt={user?.name}/>
+          </div>
+          <div>
+            <span>Logged in as: {user?.name}</span>
+          </div>
+          <div>
+            <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+          </div>
+          <div>
+            <button onClick={() => logout()}>Click to logout</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => loginWithRedirect()}>Click me to login</button>
+        </div>
+      )}
         <div className="app-container">
           <div className="navbar-container">
             Navbar
@@ -23,6 +61,8 @@ function App() {
 
             </div>
           </div>
+          <button onClick={handleClick}>Settings</button>
+
         </div>
       </ContextProvider>
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { FronteggProvider } from '@frontegg/react';
+
+
+const contextOptions = {
+    baseUrl: 'https://app-7ksv0yovdvjo.frontegg.com',
+    clientId: 'b2a2b5e3-6110-4fb8-8d1b-9194ee31618f'
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+        <App />
+    </FronteggProvider>,
 
-    <App />
- 
 )
